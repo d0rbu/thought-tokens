38f2263c0acf0835dab99cdb54db031077cb9328
d0rb <hacperu2010@gmail.com>
2024-10-12 13:42:50 -0700

    actually fix nix shell lol

--- shell.nix
+++ shell.nix
@@ -1,13 +1,25 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05") {} }:
-
-pkgs.mkShellNoCC {
-  packages = with pkgs; [
-    (python3.withPackages (ps: with ps; [
-      pip
-      torchWithCuda
-      torchvision
-      torchaudio
-      transformers
-    ]))
+let
+  # Pin to a specific nixpkgs commit for reproducibility.
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/24bb1b20a9a57175965c0a9fb9533e00e370c88b.tar.gz") {config.allowUnfree = true; };
+in pkgs.mkShell {
+  nativeBuildInputs = [
+    pkgs.python311
+    pkgs.python311Packages.torch-bin
+    pkgs.python311Packages.torchaudio-bin
+    pkgs.python311Packages.torch-audiomentations
+    pkgs.python311Packages.librosa
+    pkgs.python311Packages.jiwer
+    pkgs.python311Packages.datasets
+    pkgs.python311Packages.transformers
+    pkgs.python311Packages.evaluate
+    pkgs.python311Packages.accelerate
+    pkgs.python311Packages.pip
+    
   ];
+
+  shellHook = ''
+    echo "You are now using a NIX environment"
+    export CUDA_PATH=${pkgs.cudatoolkit}
+    echo $CUDA_PATH
+  '';
 }
