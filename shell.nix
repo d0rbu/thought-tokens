154bf4b295ea083618729eb2749ad3f4670a0a6e
d0rb <hacperu2010@gmail.com>
2024-10-16 23:02:43 -0700

    actaul training

--- shell.nix
+++ shell.nix
@@ -1,25 +1,40 @@
 let
   # Pin to a specific nixpkgs commit for reproducibility.
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/24bb1b20a9a57175965c0a9fb9533e00e370c88b.tar.gz") {config.allowUnfree = true; };
+  python = pkgs.python312;
+
+  arguably = python.pkgs.buildPythonPackage rec {
+    pname = "arguably";
+    version = "1.3.0";
+    format = "wheel";
+    doCheck = false;
+    src = pkgs.fetchPypi {
+      inherit pname version;
+      sha256 = "9261e49d0281600e9eac3fb2e31d2022dc0d002b6370461d787b20690eb2a98d";
+    };
+  };
+
+  python-with-packages = python.withPackages (ps: with ps; [
+    torch
+    torchaudio
+    torch-audiomentations
+    librosa
+    jiwer
+    datasets
+    transformers
+    evaluate
+    accelerate
+    pip
+    pytorch-lightning
+    arguably
+  ]);
 in pkgs.mkShell {
-  nativeBuildInputs = [
-    pkgs.python311
-    pkgs.python311Packages.torch-bin
-    pkgs.python311Packages.torchaudio-bin
-    pkgs.python311Packages.torch-audiomentations
-    pkgs.python311Packages.librosa
-    pkgs.python311Packages.jiwer
-    pkgs.python311Packages.datasets
-    pkgs.python311Packages.transformers
-    pkgs.python311Packages.evaluate
-    pkgs.python311Packages.accelerate
-    pkgs.python311Packages.pip
-    pkgs.python311Packages.pytorch-lightning
+  packages = [
+    python-with-packages
   ];
 
   shellHook = ''
-    echo "You are now using a NIX environment"
+    export PYTHONPATH="$(pwd):$PYTHONPATH"
     export CUDA_PATH=${pkgs.cudatoolkit}
-    echo $CUDA_PATH
   '';
 }
---
+++ train.py
@@ -0,0 +1,45 @@
+import arguably
+import importlib
+
+from typing import Any
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from pytorch_lightning import LightningModule, LightningDataModule, Trainer
+
+
+@arguably.command
+def train(
+    *args: Any,
+    script: str,
+    data: str = "minipile",
+    model: str = "meta-llama/Llama-3.2-1B",
+    warmup_steps: int = 1000,
+    lr: float = 5e-5,
+    num_thought_tokens: int = 1024,
+    max_epochs: int = 1,
+) -> None:
+    script_path = f"train.{script}"
+    script_module = importlib.import_module(script_path)
+    lightning_modules = [(name, cls) for name, cls in script_module.__dict__.items() if isinstance(cls, type) and issubclass(cls, LightningModule)]
+    assert len(lightning_modules) == 1, f"Expected exactly one LightningModule in {script_path}, found {len(lightning_modules)}: {lightning_modules}"
+
+    lightning_module_name, lightning_module = lightning_modules[0]
+
+    data_path = f"data.{data}"
+    data_module = importlib.import_module(data_path)
+    lightning_data_modules = [(name, cls) for name, cls in data_module.__dict__.items() if isinstance(cls, type) and issubclass(cls, LightningDataModule)]
+    assert len(lightning_data_modules) == 1, f"Expected exactly one LightningDataModule in {data_path}, found {len(lightning_data_modules)}: {lightning_data_modules}"
+
+    lightning_data_module_name, lightning_data_module = lightning_data_modules[0]
+
+    model = AutoModelForCausalLM.from_pretrained(model)
+    tokenizer = AutoTokenizer.from_pretrained(model)
+
+    print(f"Training {model} with {lightning_module_name} on {lightning_data_module_name}.")
+    print(f"Using {num_thought_tokens} thought tokens.")
+
+    module = lightning_module(model=model, tokenizer=tokenizer, thought_token_unembeddings=num_thought_tokens, warmup_steps=warmup_steps, lr=lr)
+    data_module = lightning_data_module()
+    trainer = Trainer(max_epochs=max_epochs)
+
+    trainer.fit(model=module, datamodule=data_module)
+    
