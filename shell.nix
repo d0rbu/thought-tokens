9a3329ed7215644d08f33b8de7dbb65d1bded95d
d0rb <hacperu2010@gmail.com>
2024-10-16 23:41:19 -0700

    omg arguably finally working

--- shell.nix
+++ shell.nix
@@ -3,15 +3,37 @@ let
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/24bb1b20a9a57175965c0a9fb9533e00e370c88b.tar.gz") {config.allowUnfree = true; };
   python = pkgs.python312;
 
+  docstring-parser = python.pkgs.buildPythonPackage rec {
+    pname = "docstring_parser";
+    version = "0.16";
+    format = "pyproject";
+    src = pkgs.fetchPypi {
+      inherit pname version;
+      sha256 = "U4vqvQrx4tsBRra9PKpSbDWjTWGvn9KIfzqKJ6c5qm4=";
+    };
+
+    propagatedBuildInputs = [
+      pkgs.python312Packages.poetry-core
+    ];
+  };
+
   arguably = python.pkgs.buildPythonPackage rec {
     pname = "arguably";
     version = "1.3.0";
-    format = "wheel";
+    format = "pyproject";
     doCheck = false;
-    src = pkgs.fetchPypi {
-      inherit pname version;
-      sha256 = "9261e49d0281600e9eac3fb2e31d2022dc0d002b6370461d787b20690eb2a98d";
+    src = pkgs.fetchFromGitHub {
+      owner = "treykeown";
+      repo = "arguably";
+      rev = "v1.3.0";
+      sha256 = "CumvE8o1mJQ/uwZ6ZikqOKOygAllTdw5jyt09DNdMUE=";
     };
+
+    # Add poetry-core as a build input
+    propagatedBuildInputs = [
+      pkgs.python312Packages.poetry-core
+      docstring-parser
+    ];
   };
 
   python-with-packages = python.withPackages (ps: with ps; [
